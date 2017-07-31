f1c7f87571e8803d17e18e1046c80ea761e2b0ce
Maxime Moreillon <m.moreillon@gmail.com>
2017-07-31 18:01:43 +0900

    first commit

---
+++ dht_pir_combo.ino
@@ -0,0 +1,83 @@
+// Libraries
+#include <ESP8266WiFi.h> // Main ESP8266 library
+#include <ArduinoOTA.h> // OTA update library
+#include <WiFiUdp.h> // Required for OTA
+#include <AsyncMqttClient.h> // MQTT library
+#include <Ticker.h> // Used when reconecting MQTT upon wifi drop
+#include <DHT.h> // Temperature and humidity sensor
+#include <ArduinoJson.h>
+
+#include "credentials.h";
+
+// Wifi
+WiFiEventHandler wifi_connect_handler;
+WiFiEventHandler wifi_disconnect_handler;
+Ticker wifi_reconnect_timer;
+
+// OTA
+#define HOSTNAME "bedroomMonitor"
+
+// MQTT
+AsyncMqttClient MQTT_client;
+Ticker MQTT_reconnect_timer;
+#define MQTT_BROKER_ADDRESS IPAddress(192, 168, 1, 2)
+#define MQTT_PORT 1883
+#define MQTT_MOTION_STATUS_TOPIC "motion/bedroom/status"
+#define MQTT_DHT_STATUS_TOPIC "dht/bedroom/status"
+#define MQTT_DEBUG_TOPIC "dht/bedroom/debug"
+#define MQTT_QOS 1
+#define MQTT_RETAIN true
+
+// Pin mapping
+#define DHT_PIN D1
+#define PIR_PIN D2
+
+// DHT variables
+DHT dht(DHT_PIN, DHT22);
+#define DHT_PUBLISH_PERIOD 300000 // [ms], 300000 is 5 minutes
+#define DHT_READ_PERIOD 10000 // [ms]
+
+long last_DHT_publish_time, last_DHT_read_time;
+float DHT_temperature = 0;
+float DHT_humidity = 0;
+
+// PIR variables
+int last_PIR_reading;
+
+void setup() {
+
+  // Mandatory initial delay
+  delay(10);
+
+  // Serial init
+  Serial.begin(115200);
+  Serial.println(); // Separate serial stream from initial gibberish
+  Serial.println(F(__FILE__ " " __DATE__ " " __TIME__));
+
+  // PIR init
+  pinMode(PIR_PIN, INPUT);
+
+  // DHT init
+  dht.begin();
+
+  wifi_setup();
+  MQTT_setup();
+  OTA_setup();
+
+  wifi_connect();
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  read_PIR();
+  read_DHT();
+
+  // Debugging
+  static boolean debugged;
+  if(millis() > 10000 && !debugged){
+    debugged = true;
+    Serial.println("debugging");
+    MQTT_client.publish(MQTT_DEBUG_TOPIC, MQTT_QOS, MQTT_RETAIN, "ON");
+    MQTT_client.publish(MQTT_DEBUG_TOPIC, MQTT_QOS, MQTT_RETAIN, "OFF");
+  }
+}
---
+++ mqtt.ino
@@ -0,0 +1,82 @@
+void MQTT_setup(){
+
+  // Callbacks
+  MQTT_client.onConnect(MQTT_connect_callback);
+  MQTT_client.onDisconnect(MQTT_disconnect_callback);
+  MQTT_client.onSubscribe(MQTT_subscribe_callback);
+  MQTT_client.onUnsubscribe(MQTT_unsubscribe_callback);
+  MQTT_client.onMessage(MQTT_message_callback);
+  MQTT_client.onPublish(MQTT_publish_callback);
+
+  // Settings
+  MQTT_client.setServer(MQTT_BROKER_ADDRESS, MQTT_PORT);
+  MQTT_client.setCredentials(MQTT_USERNAME, MQTT_PASSWORD);
+}
+
+void MQTT_connect() {
+  Serial.println("MQTT connecting...");
+
+  MQTT_reconnect_timer.detach();
+  MQTT_client.connect();
+}
+
+void MQTT_connect_callback(bool sessionPresent) {
+  Serial.println("MQTT connected");
+}
+
+
+void MQTT_disconnect_callback(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("MQTT disconnected");
+
+  if (WiFi.isConnected()) {
+    MQTT_reconnect_timer.attach(2, MQTT_connect);
+  }
+}
+
+void MQTT_subscribe_callback(uint16_t packetId, uint8_t qos) {
+  Serial.println("MQTT subscribed successfully");
+}
+
+void MQTT_unsubscribe_callback(uint16_t packetId) {
+  Serial.println("MQTT unsubscribed successfully");
+}
+
+void MQTT_message_callback(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+
+  Serial.print("MQTTT message received: ");
+  Serial.print("  topic: ");
+  Serial.print(topic);
+  Serial.print("  payload: ");
+  Serial.print(payload);
+  Serial.print("  qos: ");
+  Serial.print(properties.qos);
+  Serial.print("  dup: ");
+  Serial.print(properties.dup);
+  Serial.print("  retain: ");
+  Serial.print(properties.retain);
+  Serial.print("  len: ");
+  Serial.print(len);
+  Serial.print("  index: ");
+  Serial.print(index);
+  Serial.print("  total: ");
+  Serial.print(total);
+  Serial.println("");
+
+}
+
+void MQTT_publish_callback(uint16_t packetId) {
+  Serial.println("MQTT published successfully");
+}
+
+void MQTT_publish_DHT(float p_temperature, float p_humidity)
+{
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["temperature"] = (String)p_temperature;
+  root["humidity"] = (String)p_humidity;
+  //root.prettyPrintTo(Serial);
+  //Serial.println("");
+  char data[200];
+  root.printTo(data, root.measureLength() + 1);
+  MQTT_client.publish(MQTT_DHT_STATUS_TOPIC, MQTT_QOS, MQTT_RETAIN, data);
+}
